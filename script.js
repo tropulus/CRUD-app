51ab6bc1b77b04d47fb3fd5df3012050c35991f8
tropulus <42752904+tropulus@users.noreply.github.com>
2018-10-03 21:28:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
 
-/*
-function loadDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-     document.getElementById("changeThis").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("POST", "/pingserver", true);
-  xhttp.send();
-} */
 loadDoc2(1);
 
 setTimeout(() => {
